ade7c9c01fc12165d52744bdeec03a5e29e97e50
CheatLayer <142612335+CheatLayer@users.noreply.github.com>
2024-08-22 08:57:21 -0700

    Create app/layout.tsx

---
+++ app/layout.tsx
@@ -0,0 +1,44 @@
+import { Metadata } from 'next';
+import Footer from '@/components/ui/Footer';
+import Navbar from '@/components/ui/Navbar';
+import { Toaster } from '@/components/ui/Toasts/toaster';
+import { PropsWithChildren, Suspense } from 'react';
+import { getURL } from '@/utils/helpers';
+import 'styles/main.css';
+
+const title = 'Next.js Subscription Starter';
+const description = 'Brought to you by Vercel, Stripe, and Supabase.';
+
+export const metadata: Metadata = {
+  metadataBase: new URL(getURL()),
+  title: title,
+  description: description,
+  openGraph: {
+    title: title,
+    description: description
+  }
+};
+
+export default async function RootLayout({ children }: PropsWithChildren) {
+  const web_html = process.env.APP_NAME;
+
+  return (
+ <html lang="en">
+      
+ <html lang="en">
+      <body className="bg-black">
+        <main
+          id="skip"
+          className="min-h-[calc(100dvh-4rem)] md:min-h[calc(100dvh-5rem)]"
+        >
+          {children}
+        </main>
+        <Suspense>
+          <Toaster />
+        </Suspense>
+      </body>
+    </html>
+
+    </html>
+  );
+}

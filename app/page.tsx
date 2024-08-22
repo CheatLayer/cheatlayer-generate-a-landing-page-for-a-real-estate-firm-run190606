226c3099bb6d539a19e9de333c24dbcc227ca011
CheatLayer <142612335+CheatLayer@users.noreply.github.com>
2024-08-22 08:57:22 -0700

    Create app/page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,23 @@
+import Pricing from '@/components/ui/Pricing/Pricing';
+import { createClient } from '@/utils/supabase/server';
+import {
+  getProducts,
+  getSubscription,
+  getUser
+} from '@/utils/supabase/queries';
+
+export default async function PricingPage() {
+  const supabase = createClient();
+  const [user, products, subscription] = await Promise.all([
+    getUser(supabase),
+    getProducts(supabase),
+    getSubscription(supabase)
+  ]);
+  const web_html = process.env.APP_NAME;
+
+  return (
+      <div
+      dangerouslySetInnerHTML={{ __html: web_html as string }}
+    />
+  );
+}
